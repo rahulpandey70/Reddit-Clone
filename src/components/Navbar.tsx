28ed9d79b2ead81cf4a8c7f3795119515c86b2d4
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-19 17:16:34 +0530

    Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
 		<div className="fixed top-0 inset-x-0 h-fix bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
 			<div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
 				<Link href="/" className="flex gap-2 items-center">
-					<p className=" text-zinc-700 text-sm font-medium md:block">
+					<p className=" text-zinc-700 text-lg font-semibold md:block tracking-wider">
 						AskQuery
 					</p>
 				</Link>
