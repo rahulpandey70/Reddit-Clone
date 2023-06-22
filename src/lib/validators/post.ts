3b7d1a49d391b81198926c2a68e1b83e31cb1c81
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-22 16:32:43 +0530

    Update

--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const PostValidator = z.object({
 	title: z
 		.string()
-		.min(10, { message: "Title mustbe at least 10 characters." })
+		.min(5, { message: "Title mustbe at least 5 characters." })
 		.max(150, { message: "Title should not be longer than 150 characters." }),
 	subredditId: z.string(),
 	content: z.any(),
