c1705a461d54733d47d582d9f77556e93b724ddd
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-24 22:40:40 +0530

    update

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { ExtendedPost } from "@/types/db";
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import { useIntersection } from "@mantine/hooks";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import Post from "./Post";
-import { Subreddit } from "@prisma/client";
 
 interface PostFeedProps {
 	initialPosts: ExtendedPost[];
@@ -41,6 +40,12 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
 			initialData: { pages: [initialPosts], pageParams: [1] },
 		}
 	);
+
+	useEffect(() => {
+		if (entry?.isIntersecting) {
+			fetchNextPage();
+		}
+	}, [entry, fetchNextPage]);
 
 	const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
 
