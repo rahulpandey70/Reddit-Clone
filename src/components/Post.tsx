fdb301b7557d131cd8d47e33e7771680c122cd06
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-24 22:09:19 +0530

    Update

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,6 +4,9 @@ import { Post, User, Vote } from "@prisma/client";
 import { MessageSquare } from "lucide-react";
 import { FC, useRef } from "react";
 import EditorOutput from "./EditorOutput";
+import PostVoteClient from "./post-vote/PostVoteClient";
+
+type PartialVote = Pick<Vote, "type">;
 
 interface PostProps {
 	subredditName: string;
@@ -12,15 +15,27 @@ interface PostProps {
 		votes: Vote[];
 	};
 	commentAmt: number;
+	votesAmt: number;
+	currentVote?: PartialVote;
 }
 
-const Post: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
+const Post: FC<PostProps> = ({
+	subredditName,
+	post,
+	commentAmt,
+	votesAmt,
+	currentVote,
+}) => {
 	const pRef = useRef<HTMLDivElement>(null);
 
 	return (
 		<div className="rounded-md bg-white shadow">
 			<div className="px-6 py-4 flex justify-between">
-				{/* TODO: PostVote */}
+				<PostVoteClient
+					initialVotesAmt={votesAmt}
+					postId={post.id}
+					initialVote={currentVote?.type}
+				/>
 
 				<div className="w-0 flex-1">
 					<div className="max-h-40 mt-1 text-xs text-gray-500">
@@ -53,6 +68,7 @@ const Post: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
 							<div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent" />
 						) : null}
 					</div>
+					P
 				</div>
 			</div>
 
