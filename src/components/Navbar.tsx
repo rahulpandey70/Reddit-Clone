297a59bc44b9d8f61ba9b8f42017d5a2a39da5d4
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-25 22:30:23 +0530

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import UserAccountNav from "./UserAccountNav";
+import SearchBar from "./SearchBar";
 
 const Navbar = async () => {
 	const session = await getAuthSession();
@@ -15,7 +16,7 @@ const Navbar = async () => {
 					</p>
 				</Link>
 
-				{/* serach bar */}
+				<SearchBar />
 
 				{session?.user ? (
 					<UserAccountNav user={session.user} />
